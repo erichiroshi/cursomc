6f63ced6a1f3944cf4096ab31dadab7db88e1d7e
erichiroshi <erichiroshi@gmail.com>
2020-12-29 10:26:13 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/erichiroshi/cursomc/domain/Produto.java
+++ src/main/java/com/erichiroshi/cursomc/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.EqualsAndHashCode;
@@ -18,23 +20,26 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@EqualsAndHashCode.Include
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter private Integer id;
 	
 	@Getter @Setter private String nome;
+	@Getter @Setter private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	@Getter List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+	joinColumns = @JoinColumn(name = "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@Getter	List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-	
-	
 }
