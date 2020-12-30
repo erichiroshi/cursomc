9949b2458147f36ea5da1b6a332ab6dbdd0d287e
erichiroshi <erichiroshi@gmail.com>
2020-12-30 09:10:35 -0400

    Personalizando o retorno da validação

--- src/main/java/com/erichiroshi/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/erichiroshi/cursomc/resources/exceptions/StandardError.java
@@ -8,12 +8,10 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
