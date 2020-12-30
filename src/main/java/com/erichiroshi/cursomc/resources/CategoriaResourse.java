c5a9da5345163ed76610d86fb55793484298b1ca
erichiroshi <erichiroshi@gmail.com>
2020-12-29 20:52:15 -0400

    Inserindo nova Categoria com POST e GET listar Categorias

--- src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
@@ -1,11 +1,18 @@
 package com.erichiroshi.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erichiroshi.cursomc.domain.Categoria;
 import com.erichiroshi.cursomc.services.CategoriaService;
@@ -17,10 +24,23 @@ public class CategoriaResourse {
 	@Autowired
 	private CategoriaService service;
 
+	@GetMapping
+	public ResponseEntity<List<Categoria>> listar(){
+		List<Categoria> list = service.listar();
+		return ResponseEntity.ok().body(list);
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
--- src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
+++ src/main/java/com/erichiroshi/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.erichiroshi.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,17 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
+	public List<Categoria> listar(){
+		return repo.findAll();
+	}
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
