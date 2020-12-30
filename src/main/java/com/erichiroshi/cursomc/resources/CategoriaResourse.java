6520bb2cfbaa8b4ed05c76f4c8560446f9a75d9d
erichiroshi <erichiroshi@gmail.com>
2020-12-30 08:30:37 -0400

    Validação sintática com Bean Validation

--- src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -44,14 +46,16 @@ public class CategoriaResourse {
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Categoria> insert(@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDto(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Categoria> update(@PathVariable Integer id, @RequestBody Categoria obj) {
+	public ResponseEntity<Categoria> update(@PathVariable Integer id,@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDto(objDto);
 		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
