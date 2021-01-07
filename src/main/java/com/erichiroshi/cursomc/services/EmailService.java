0afa73a278d6263214520ec24c1458662d638525
erichiroshi <erichiroshi@gmail.com>
2021-01-07 16:39:59 -0400

    Email HTML

--- src/main/java/com/erichiroshi/cursomc/services/EmailService.java
+++ src/main/java/com/erichiroshi/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.erichiroshi.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.erichiroshi.cursomc.domain.Pedido;
@@ -7,6 +9,10 @@ import com.erichiroshi.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
