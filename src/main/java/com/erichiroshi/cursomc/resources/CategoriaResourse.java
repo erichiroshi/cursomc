edd07e1ad1fca820c2cbaa413f44f63cc4f1bc87
erichiroshi <erichiroshi@gmail.com>
2021-01-08 20:27:53 -0400

    Autorizando endpoints para perfis especificos

--- src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/erichiroshi/cursomc/resources/CategoriaResourse.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,6 +46,7 @@ public class CategoriaResourse {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<Categoria> insert(@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDto(objDto);
@@ -53,6 +55,7 @@ public class CategoriaResourse {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Categoria> update(@PathVariable Integer id,@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDto(objDto);
@@ -60,6 +63,7 @@ public class CategoriaResourse {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
 		service.deleteById(id);
