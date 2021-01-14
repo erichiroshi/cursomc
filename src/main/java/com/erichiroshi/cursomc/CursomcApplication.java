3693a6aa64e2e424ce971b402a17cdebc0cd77f6
erichiroshi <erichiroshi@gmail.com>
2021-01-14 13:17:46 -0400

    Enviando imagem via endpoint

--- src/main/java/com/erichiroshi/cursomc/CursomcApplication.java
+++ src/main/java/com/erichiroshi/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.erichiroshi.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.erichiroshi.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,9 +13,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		s3Service.uploadFile(
-				"C:\\Users\\erich\\OneDrive\\Área de Trabalho\\Udemy\\Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB\\02 - diagrama.png");
 
 	}
 }
--- src/main/java/com/erichiroshi/cursomc/resources/ClienteResourse.java
+++ src/main/java/com/erichiroshi/cursomc/resources/ClienteResourse.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erichiroshi.cursomc.domain.Cliente;
@@ -82,4 +83,10 @@ public class ClienteResourse {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@PostMapping(value = "/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
+
 }
